046cd1c8592daed925a0d6f188bddf2e7752f91f
Rostislav Kulik <45662763+r-kulik@users.noreply.github.com>
2024-08-20 12:28:46 +0500

    Файлы загружаются на сервер и просто отправляются в папку

--- gis_app/urls.py
+++ gis_app/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.hello_world, name='hello_world'),
+    path('import/', views.import_view, name='Импорт LAS-файлов'),
+    path('upload/', views.upload_file, name='upload')
 ]
