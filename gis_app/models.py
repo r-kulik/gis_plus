ef17f088f3a08317158965eeebf28e4893b8504a
Rostislav Kulik <45662763+r-kulik@users.noreply.github.com>
2024-08-20 15:45:15 +0500

    Создана схема базы данных, рандомные данные загружены

--- gis_app/models.py
+++ gis_app/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+class Companies(models.Model):
+    companyId = models.AutoField(primary_key=True)
+    companyName = models.TextField()
+
+class Fields(models.Model):  # Corrected from models.model to models.Model
+    fieldId = models.AutoField(primary_key=True)
+    fieldName = models.TextField()
+
+class Locations(models.Model):
+    locationId = models.AutoField(primary_key=True)
+    field = models.ForeignKey(to=Fields, on_delete=models.CASCADE)
+
+class CurveMetrics(models.Model):
+    metricName = models.TextField()
+
+class Files(models.Model):
+    """
+    Здесь и далее при описании моделей используется CamelNotation
+    """
+    fileId = models.AutoField(primary_key=True)
+    filePath = models.FilePathField()
+    fileVersion = models.CharField(max_length=5)
+    startDepth = models.FloatField()
+    stopDepth = models.FloatField()
+    datetime = models.DateTimeField()
+    company = models.ForeignKey(to=Companies, on_delete=models.CASCADE)
+    location = models.ForeignKey(to=Locations, on_delete=models.CASCADE)
+    metrics = models.ManyToManyField(CurveMetrics)
--- gis_app/views.py
+++ gis_app/views.py
@@ -2,9 +2,64 @@ from django.shortcuts import render # type: ignore
 from django.http import HttpResponse, HttpRequest, JsonResponse # type: ignore
 from django.views.decorators.csrf import csrf_exempt # type: ignore
 import os
+import random
+import string
+from datetime import datetime
+from .models import Companies, Fields, Locations, CurveMetrics, Files
 
 
 def hello_world(request):
+    
+
+    def random_string(length=10):
+        return ''.join(random.choice(string.ascii_letters) for _ in range(length))
+
+    def random_number(start=0, end=100):
+        return random.uniform(start, end)
+
+    def create_dummy_data():
+        # Create dummy Companies
+        company1 = Companies.objects.create(companyName=random_string())
+        company2 = Companies.objects.create(companyName=random_string())
+
+        # Create dummy Fields
+        field1 = Fields.objects.create(fieldName=random_string())
+        field2 = Fields.objects.create(fieldName=random_string())
+
+        # Create dummy Locations
+        location1 = Locations.objects.create(field=field1)
+        location2 = Locations.objects.create(field=field2)
+
+        # Create dummy CurveMetrics
+        metric1 = CurveMetrics.objects.create(metricName=random_string())
+        metric2 = CurveMetrics.objects.create(metricName=random_string())
+
+        # Create dummy Files
+        file1 = Files.objects.create(
+            filePath=random_string(),
+            fileVersion=random_string(5),
+            startDepth=random_number(),
+            stopDepth=random_number(),
+            datetime=datetime.now(),
+            company=company1,
+            location=location1
+        )
+        file1.metrics.add(metric1, metric2)
+
+        file2 = Files.objects.create(
+            filePath=random_string(),
+            fileVersion=random_string(5),
+            startDepth=random_number(),
+            stopDepth=random_number(),
+            datetime=datetime.now(),
+            company=company2,
+            location=location2
+        )
+        file2.metrics.add(metric1)
+
+    # Call the function to create dummy data
+    create_dummy_data()
+        
     return HttpResponse("Hello, World!")
 
 
@@ -15,6 +70,10 @@ def import_view(request: HttpRequest) -> HttpResponse:
 
 @csrf_exempt
 def upload_file(request):
+
+    FileTOCheck = Files(
+
+    )
     if request.method == 'POST' and request.FILES.getlist('files'):
         files = request.FILES.getlist('files')
         file_data = []
