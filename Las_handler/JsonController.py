6e23a0f8e6f7773ad98227210448bb566bc01e23
Eugeniy <zensorok29@gmail.com>
2024-08-21 08:10:05 +0300

    Aded Json controller

---
+++ Las_handler/JsonController.py
@@ -0,0 +1,27 @@
+import json
+
+class JsonController:
+    def __init__(self):
+        with open("eng_com.json", 'r') as json_file:
+            self.eng_com = json.load(json_file)
+            
+        with open("rus_com.json", 'r') as json_file:
+            self.rus_com = json.load(json_file)
+            
+        with open("eng.json", 'r') as json_file:
+            self.eng = json.load(json_file)
+            
+        with open("rus.json", 'r') as json_file:
+            self.rus = json.load(json_file)
+            
+    def get_eng_com_element(self, mnemonic: str) -> str:
+        return self.eng_com.get(mnemonic, "Element not found")
+    
+    def get_rus_com_element(self, mnemonic: str) -> str:
+        return self.rus_com.get(mnemonic, "Element not found")
+    
+    def get_eng_origin_mnemonic(self, mnemonic: str) -> str:
+        return self.eng.get(mnemonic, "Element not found")
+    
+    def get_rus_origin_mnemonic(self, mnemonic: str) -> str:
+        return self.rus.get(mnemonic, "Element not found")
