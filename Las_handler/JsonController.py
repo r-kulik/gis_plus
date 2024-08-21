efd207df931309a6a6720fd5dc74ffa6facb9214
Eugeniy <zensorok29@gmail.com>
2024-08-21 09:18:40 +0300

    Aded Interfaces

--- Las_handler/JsonController.py
+++ Las_handler/JsonController.py
@@ -2,16 +2,16 @@ import json
 
 class JsonController:
     def __init__(self):
-        with open("eng_com.json", 'r') as json_file:
+        with open("Json/eng_com.json", 'r') as json_file:
             self.eng_com = json.load(json_file)
             
-        with open("rus_com.json", 'r') as json_file:
+        with open("Json/rus_com.json", 'r') as json_file:
             self.rus_com = json.load(json_file)
             
-        with open("eng.json", 'r') as json_file:
+        with open("Json/eng.json", 'r') as json_file:
             self.eng = json.load(json_file)
             
-        with open("rus.json", 'r') as json_file:
+        with open("Json/rus.json", 'r') as json_file:
             self.rus = json.load(json_file)
             
     def get_eng_com_element(self, mnemonic: str) -> str:
---
+++ Las_handler/SuperLas.py
@@ -0,0 +1,20 @@
+class SuperLas:
+    def __init__(self):
+        pass
+    
+    def process_file(self, file_path: str) -> dict:
+        return {"status" : "ok",
+                "description": "Всё отлично",
+                "features": 
+                    {
+                        "start_depth": 100,
+                        "stop_depth": 1000,
+                        "version": "1.1",
+                        "datetime": None,
+                        "company": "Шазпром",
+                        "well": "5555",
+                        "mnemonic_list_rus": ['ПС', "ИН", "A"],
+                        "mnemonic_list_eng": ['D', "O", "G"],
+                        "file_path": "Encoded/1.las",
+                        
+                    }}
