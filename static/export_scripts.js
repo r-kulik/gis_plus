7dbb163cd585ff4dd84d77871641116a3ffc3d39
KonstantinPetrovichQWERTY <skatkov24@mail.ru>
2024-08-22 06:55:08 +0500

    avoid checkbox

--- static/export_scripts.js
+++ static/export_scripts.js
@@ -151,7 +151,10 @@ $(document).ready(
         //     }
         // });
 
-        $('#fileTable tbody').on('click', 'tr', function() {
+        $('#fileTable tbody').on('click', 'tr', function(event) {
+            if ($(event.target).closest('td').hasClass('ignore-click')) {
+                return; // Ignore the click event
+            }
             const fileId = $(this).data('fileId');
             const fileName = $(this).data('fileName');
             console.log('Clicked row file ID:', fileId);
